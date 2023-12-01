0b44e45c45ef815e0192333a4f9e9016a65b780e
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2023-12-01 11:29:41 -0300

    criação formulário

---
+++ src/views/Register.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h2>Registro de usuário</h2>
+        <hr>
+
+
+        <div class="columns is-mobile is-centered">
+            <div class="column is-half">
+                <p>Nome</p>
+                <input type="text" class="input" name="" id="" placeholder="Nome do usuário">
+                <p>E-mail</p>
+                <input type="email" class="input" name="" id="" placeholder="email@email.com">
+                <p>Senha</p>
+                <input type="password" class="input" name="" id="" placeholder="*********">
+                <br><br>
+                <button class="button is-success">Cadastrar</button>
+
+            </div>
+        </div>
+
+        
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'RegistroComponent'
+}
+</script>
+
+<style scoped>
+
+</style>
