dbf5080d87c9c67d2a98f40d7f75231667eb2564
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2023-12-03 17:25:53 -0300

    renderização tabela usuários

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,12 +1,69 @@
 <template>
     <div>
         <h1>Painel Adm</h1>
+        <div>
+            
+        </div>
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>E-mail</th>
+                    <th>Cargo</th>
+                    <th>Ações</th>            
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="user in users" :key="user.id">
+                    <td>{{ user.name }}</td>
+                    <td>{{ user.email }}</td>
+                    <td>{{ getRole(user.role) }}</td>
+                    <td>
+                        <button class="button is-success">Editar</button> |
+                        <button class="button is-danger">Deletar</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-    name: 'UsersAdmin'
+    name: 'UsersAdmin',
+    created(){
+        var req = {
+            headers: {
+                Authorization: "Bearer " + localStorage.getItem('token')
+            }
+        }
+
+        axios.get('http://localhost:8686/user', req).then(res => {
+            console.log(res);
+            this.users = res.data;
+        }).catch(error => {
+            console.log(error);
+        })
+    },
+    data(){
+        return{
+            users: []
+        }
+    },
+    methods: {
+        getRole(role) {
+            if(role === 0) {
+                return "Usuário";
+            } else if(role === 1) {
+                return "Admin";
+            } else {
+                return "Outro";
+            }
+        }
+    }
 }
 </script>
 
